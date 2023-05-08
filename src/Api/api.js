117d962ca12e2fb494345ef8c355706323b60ae9
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-05-08 14:51:05 +0530

    minor fix

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 // const API_BASE_URL = "http://provider.palmito.duckdns.org:31378";
 
-const API_BASE_URL = "https://web3-notes.live";
+// const API_BASE_URL = "https://web3-notes.live";
+
+const API_BASE_URL = "http://localhost:5000";
 
 export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
-  withCredentials: true,
 });
 
 export const apiPrivateInstance = axios.create({
